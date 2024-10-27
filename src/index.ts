cfcff8790d48da9760e2dbc98ce5e313b71ddc87
avrhahm sason <avi6190i@gmail.com>
2024-10-27 17:32:55 +0200

    'build'

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import * as Assets from "./assets";
-import * as Components from "./components";
-import * as Helpers from "./helpers";
-import * as Hooks from "./hooks";
-import * as Types from "./types";
-
-export const assets = Assets;
-export const components = Components;
-export const helpers = Helpers;
-export const hooks = Hooks;
-export const types = Types;
+export * from './svg'
